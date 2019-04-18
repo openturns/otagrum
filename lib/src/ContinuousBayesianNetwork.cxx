762829fc26686b89fba8b59eba2deec5700b2dbd
regislebrun <regis.lebrun@m4x.org>
2019-04-18 10:07:52 +0200

    Started ContinuousBayesianNetwork

---
+++ lib/src/ContinuousBayesianNetwork.cxx
@@ -0,0 +1,168 @@
+//                                               -*- C++ -*-
+/**
+ *  @brief The ContinuousBayesianNetwork distribution
+ *
+ *
+ *  Copyright 2010-2019 Airbus-LIP6-Phimeca
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <cmath>
+#include "otagrum/ContinuousBayesianNetwork.hxx"
+
+#include <openturns/OSS.hxx>
+#include <openturns/Exception.hxx>
+#include <openturns/RandomGenerator.hxx>
+#include <openturns/SpecFunc.hxx>
+#include <openturns/DistFunc.hxx>
+#include <openturns/Uniform.hxx>
+#include <openturns/PersistentObjectFactory.hxx>
+
+
+namespace OTAGRUM
+{
+
+CLASSNAMEINIT(ContinuousBayesianNetwork);
+
+static const OT::Factory<ContinuousBayesianNetwork> Factory_ContinuousBayesianNetwork;
+
+/* Default constructor */
+ContinuousBayesianNetwork::ContinuousBayesianNetwork()
+  : OT::ContinuousDistribution()
+  , dag_()
+  , jointDistributions_(0)
+{
+  setName( "ContinuousBayesianNetwork" );
+}
+
+/* Parameters constructor */
+ContinuousBayesianNetwork::ContinuousBayesianNetwork(const NamedDAG & dag,
+						     const DistributionCollection & jointDistributions)
+  : OT::ContinuousDistribution()
+  , dag_(dag)
+  , jointDistributions_(0)
+{
+  setName( "ContinuousBayesianNetwork" );
+  computeRange();
+  setDimension(getRange().getDimension());
+}
+
+/* Comparison operator */
+OT::Bool ContinuousBayesianNetwork::operator ==(const ContinuousBayesianNetwork & other) const
+{
+  if (this == &other) return true;
+  return (dag_ == other.dag_) && (jointDistributions_ == other.jointDistributions_);
+}
+
+OT::Bool ContinuousBayesianNetwork::equals(const DistributionImplementation & other) const
+{
+  const ContinuousBayesianNetwork* p_other = dynamic_cast<const ContinuousBayesianNetwork*>(&other);
+  return p_other && (*this == *p_other);
+}
+
+/* String converter */
+OT::String ContinuousBayesianNetwork::__repr__() const
+{
+  OT::OSS oss(true);
+  oss << "class=" << ContinuousBayesianNetwork::GetClassName()
+      << " name=" << getName()
+      << " dimension=" << getDimension()
+      << " dag=" << dag_
+      << " jointDistributions=" << jointDistributions_;
+  return oss;
+}
+
+OT::String ContinuousBayesianNetwork::__str__(const OT::String & offset) const
+{
+  OT::OSS oss(false);
+  oss << offset << getClassName() << "(dag=" << dag_ << ", joint distributions=" << jointDistributions_ << ")";
+  return oss;
+}
+
+/* Virtual constructor */
+ContinuousBayesianNetwork * ContinuousBayesianNetwork::clone() const
+{
+  return new ContinuousBayesianNetwork(*this);
+}
+
+/* Compute the numerical range of the distribution given the parameters values */
+void ContinuousBayesianNetwork::computeRange()
+{
+  OT::Point lower(0);
+  OT::Point upper(0);
+  for (OT::UnsignedInteger i = 0; i < dag_.getNodes().getSize(); ++i)
+    {
+      // Check if the current node is a root node
+      if (dag_.getParents().getSize() == 0)
+	{
+	  lower.add(jointDistributions_[i].getLowerBound()[0]);
+	  upper.add(jointDistributions_[i].getUpperBound()[0]);
+	}
+    } // i
+  setRange(OT::Interval(lower, upper));
+}
+
+/* Get one realization of the distribution */
+OT::Point ContinuousBayesianNetwork::getRealization() const
+{
+  throw OT::NotYetImplementedException(HERE) << "In ContinuousBayesianNetwork::getRealization()";
+}
+
+/* Get the PDF of the distribution */
+OT::Scalar ContinuousBayesianNetwork::computePDF(const OT::Point & point) const
+{
+  const OT::Indices order(dag_.getTopologicalOrder());
+  OT::Scalar pdf = 1.0;
+  for (OT::UnsignedInteger i = 0; i < order.getSize(); ++i)
+    {
+      const OT::UnsignedInteger globalI = order[i];
+      const OT::Scalar x = point[globalI];
+      const OT::Indices parents(dag_.getParents(globalI));
+      const OT::UnsignedInteger conditioningDimension(parents.getSize());
+      OT::Point y(conditioningDimension);
+      for (OT::UnsignedInteger j = 0; j < conditioningDimension; ++j)
+	y[j] = point[order[j]];
+      pdf *= jointDistributions_[i].computeConditionalPDF(x, y);
+    } // i
+  return pdf;
+}
+
+/* Method save() stores the object through the StorageManager */
+void ContinuousBayesianNetwork::save(OT::Advocate & adv) const
+{
+  OT::ContinuousDistribution::save(adv);
+  adv.saveAttribute( "cliquesCollection_", cliquesCollection_ );
+  adv.saveAttribute( "separatorsCollection_", separatorsCollection_ );
+  adv.saveAttribute( "copulaSample_", copulaSample_ );
+  adv.saveAttribute( "binNumber_", binNumber_ );
+  adv.saveAttribute( "logBetaMarginalFactors_", logBetaMarginalFactors_ );
+  adv.saveAttribute( "logFactors_", logFactors_ );
+}
+
+/* Method load() reloads the object from the StorageManager */
+void ContinuousBayesianNetwork::load(OT::Advocate & adv)
+{
+  OT::ContinuousDistribution::load(adv);
+  adv.loadAttribute( "cliquesCollection_", cliquesCollection_ );
+  adv.loadAttribute( "separatorsCollection_", separatorsCollection_ );
+  adv.loadAttribute( "copulaSample_", copulaSample_ );
+  adv.loadAttribute( "binNumber_", binNumber_ );
+  adv.loadAttribute( "logBetaMarginalFactors_", logBetaMarginalFactors_ );
+  adv.loadAttribute( "logFactors_", logFactors_ );
+  computeRange();
+}
+
+} /* namespace OTAGR */
