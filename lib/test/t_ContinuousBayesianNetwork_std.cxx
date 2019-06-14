df9026425a496c7fcb19fc8b081bea5c360e3517
regislebrun <regis.lebrun@m4x.org>
2019-06-14 20:07:34 +0200

    Fixed ContinuousBayesianNetwork

--- lib/test/t_ContinuousBayesianNetwork_std.cxx
+++ lib/test/t_ContinuousBayesianNetwork_std.cxx
@@ -1,21 +1,18 @@
 #include <openturns/OT.hxx>
 
 #include "otagrum/NamedDAG.hxx"
+#include "otagrum/ContinuousBayesianNetwork.hxx"
 #include <iostream>
 
 void testConstructor() {
   const auto proto = "A->B->C->D;E->A->C<-E";
   const auto bn = gum::BayesNet<double>::fastPrototype(proto);
-
   std::cout << "      proto : " << proto << std::endl;
   std::cout << "         BN : " << bn << std::endl << std::endl;
-
   OTAGRUM::NamedDAG ndag(bn);
-
   std::cout << "       size : " << ndag.getSize() << std::endl;
   std::cout << "       desc : " << ndag.getDescription() << std::endl
             << std::endl;
-
   std::cout << "      nodes : " << ndag.getNodes() << std::endl;
   for (const auto &nod : ndag.getNodes())
     {
@@ -23,42 +20,58 @@ void testConstructor() {
       std::cout << "children(" << nod << ") : " << ndag.getChildren(nod) << std::endl;
     }
 
-  std::cout << "topologicalOrder : " << ndag.getTopologicalOrder() << std::endl;
-
-  std::cout << std::endl << ndag.__str__("+++ +++ |") << std::endl;
-
-  std::cout << std::endl << ndag.toDot();
-
-  OT::Log::Show(OT::Log::ALL);
-  OT::Study study("tmp.xml");
-  study.add("ndag", ndag);
-  study.save();
-
-  std::cout << "\n------------------\n";
-  OT::Study study2("tmp.xml");
-  study2.load();
-  std::cout << "Labels=" << study2.printLabels() << std::endl;
-  std::cout << "\n------------------\n";
-
-  OTAGRUM::NamedDAG dag;
-  std::cout << "Before fill, " << dag.getClassName() << ", " << dag.getName() << std::endl;
-  
-  study2.fillObject("ndag", dag);
-
-  std::cout << "        size : " << dag.getSize() << std::endl;
-  std::cout << "       desc : " << dag.getDescription() << std::endl
-            << std::endl;
-
-  std::cout << "      nodes : " << dag.getNodes() << std::endl;
-  std::cout << " parents(0) : " << dag.getParents(0) << std::endl;
-  std::cout << "children(0) : " << dag.getChildren(0) << std::endl;
-
-  std::cout << "topologicalOrder : " << dag.getTopologicalOrder() << std::endl;
-
-  std::cout << std::endl << dag.__str__("+++ +++ |") << std::endl;
-
-  std::cout << std::endl << dag.toDot();
-
+  OT::Indices order = ndag.getTopologicalOrder();
+  std::cout << "topologicalOrder : " << order << std::endl;
+  OT::Collection<OT::Distribution> jointDistributions(5);
+  for (OT::UnsignedInteger i = 0; i < order.getSize(); ++i)
+    {
+      const OT::UnsignedInteger d = 1 + ndag.getParents(i).getSize();
+      std::cout << "i=" << i << ", d=" << d << std::endl;
+      /*if (d == 2) jointDistributions[i] = OT::GumbelCopula(5.0);
+	else*/
+	{
+	  /*OT::CorrelationMatrix R(d);
+	    for (OT::UnsignedInteger i = 0; i < d; ++i)
+	      for (OT::UnsignedInteger j = 0; j < i; ++j)
+	        R(i, j) = 0.5 / d;
+		jointDistributions[i] = OT::NormalCopula(R);*/
+	  OT::Point theta(d + 1);
+	  for (OT::UnsignedInteger i = 0; i <= d; ++i)
+	    theta[i] = std::sqrt((1.0 + i) / d);
+	  jointDistributions[i] = OT::Dirichlet(theta).getCopula();
+	}
+      //jointDistributions[i] = OT::KernelSmoothing().build(OT::NormalCopula(R).getSample(10)).getCopula();
+    }
+  OTAGRUM::ContinuousBayesianNetwork cbn(ndag, jointDistributions);
+  std::cout << "cbn=" << cbn << std::endl;
+  std::cout << "cbn pdf=" << cbn.computePDF(OT::Point(cbn.getDimension(), 0.5)) << std::endl;
+  std::cout << "cbn realization=" << cbn.getRealization() << std::endl;
+  const OT::UnsignedInteger size = 10000;
+  const OT::Sample sampleLearn(cbn.getSample(size));
+  const OT::Sample sample(cbn.getSample(size));
+  std::cout << "cbn sample=" << sample << std::endl;
+  OT::Scalar logL = 0.0;
+  for (OT::UnsignedInteger i = 0; i < size; ++i)
+    logL += std::log(cbn.computePDF(sample[i]));
+  logL /= size;
+  std::cout << "log-l=" << logL << std::endl;
+  OT::NormalCopula copula(OT::NormalCopulaFactory().buildAsNormalCopula(sampleLearn));
+  std::cout << "copula=" << copula << std::endl;
+  std::cout << "log-l=" << copula.computeLogPDF(sample).computeMean()[0] << std::endl;
+  {
+  OT::Graph gr;
+  OT::Pairs pairs(sample);
+  pairs.setPointStyle("dot");
+  gr.add(pairs);
+  gr.draw("pairs.png", 800, 820);
+  }
+  {
+  OT::Graph gr;
+  OT::Pairs pairs(copula.getSample(size));
+  pairs.setPointStyle("dot");
+  gr.add(pairs);
+  gr.draw("pairsCopula.png", 800, 820);
+  }
 }
 
 int main(void) {
