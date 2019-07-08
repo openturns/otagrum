e675f4ab24e490e918bfdc08607d6d0c7f2a7053
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2019-07-08 15:25:56 +0200

    updating test for continuousPC

--- lib/test/t_JunctionTreeBernsteinCopula_std.cxx
+++ lib/test/t_JunctionTreeBernsteinCopula_std.cxx
@@ -5,14 +5,13 @@
 
 #include <openturns/OT.hxx>
 
-#include "otagrum/NamedJunctionTree.hxx"
 #include "otagrum/JunctionTreeBernsteinCopula.hxx"
+#include "otagrum/NamedJunctionTree.hxx"
 #include "otagrum/otagrum.hxx"
 
-void testOK()
-{
+void testOK() {
   auto bn =
-    gum::BayesNet<double>::fastPrototype("0->1->2->3;1->4->3;4->5;6->5;7->5");
+      gum::BayesNet<double>::fastPrototype("0->1->2->3;1->4->3;4->5;6->5;7->5");
   /*
    *   0
    *    \
@@ -27,74 +26,46 @@ void testOK()
 
   auto jtagr = ie.junctionTree();
   std::vector<std::string> names;
-  for (const auto &elt : bn.nodes())
-  {
+  for (const auto &elt : bn.nodes()) {
     names.push_back(bn.variable(elt).name());
   }
   auto jt = OTAGRUM::NamedJunctionTree(*jtagr, names);
   unsigned int dim = jt.getSize();
   OT::CovarianceMatrix C(dim);
-  for (unsigned int i = 0; i < dim; ++i)
-  {
+  for (unsigned int i = 0; i < dim; ++i) {
     for (unsigned int j = 0; j < i; ++j)
       C(i, j) = 1.0;
     C(i, i) = 2.0;
   }
+  double entropyMC1, entropyMC2;
   OT::RandomGenerator::SetSeed(0);
-  std::cout<<"Generator "<<OT::RandomGenerator::GetState()<<std::endl;
   OT::Sample copulaSample = OT::Normal(OT::Point(dim), C).getSample(1000);
-  std::cout<<copulaSample<<std::endl;
-  std::cout<<"Generator "<<OT::RandomGenerator::GetState()<<std::endl;
-  std::cout<<"=========================="<<std::endl;
-  std::cout<<"=========================="<<std::endl;
-
-
   {
-    OT::RandomGenerator::SetSeed(0);
-    std::cout<<"Generator "<<OT::RandomGenerator::GetState()<<std::endl;
+    OT::RandomGenerator::SetSeed(33);
     OTAGRUM::JunctionTreeBernsteinCopula copula(jt, copulaSample, 5, false);
-    std::cout << "copula=" << copula << std::endl;
-    OT::Sample sample = copula.getSample(1);
-    std::cout << "sample=" << sample << std::endl;
+    OT::Sample sample = copula.getSample(1000);
     OT::Sample pdf = copula.computePDF(sample);
-    std::cout << "pdf=" << pdf << std::endl;
-    double entropyMC = -copula.computeLogPDF(copula.getSample(1000)).computeMean()[0];
-    std::cout << "entropy (MC)=" << entropyMC << std::endl;
-    std::cout<<"Generator "<<OT::RandomGenerator::GetState()<<std::endl;
+    entropyMC1 = -copula.computeLogPDF(copula.getSample(1000)).computeMean()[0];
+    std::cout << "entropy (MC)=" << entropyMC1 << std::endl;
   }
   {
-    OT::RandomGenerator::SetSeed(0);
-    std::cout<<"Generator "<<OT::RandomGenerator::GetState()<<std::endl;
-    OTAGRUM::JunctionTreeBernsteinCopula copula(jt, jt.getOrderMaxFirst(), copulaSample, 5, false);
-    std::cout << "copula=" << copula << std::endl;
-    OT::Sample sample = copula.getSample(10);
-    std::cout << "sample=" << sample << std::endl;
+    OT::RandomGenerator::SetSeed(33);
+    OTAGRUM::JunctionTreeBernsteinCopula copula(jt, jt.getOrderMaxFirst(),
+                                                copulaSample, 5, false);
+    OT::Sample sample = copula.getSample(1000);
     OT::Sample pdf = copula.computePDF(sample);
-    std::cout << "pdf=" << pdf << std::endl;
-    double entropyMC = -copula.computeLogPDF(copula.getSample(1000)).computeMean()[0];
-    std::cout << "entropy (MC)=" << entropyMC << std::endl;
-    std::cout<<"Generator "<<OT::RandomGenerator::GetState()<<std::endl;
-    if (false)
-    {
-      OT::Indices indices(2);
-      for (unsigned int i = 0; i < dim; ++i)
-      {
-        indices[0] = i;
-        for (unsigned int j = 0; j < i; ++j)
-        {
-          indices[1] = j;
-          OT::Graph graph = copula.getMarginal(indices).drawPDF();
-          graph.draw(OT::String(OT::OSS() << "marginal_" << i << "_" << j << "_pdf.png"), 600, 620, 1);
-        } // j
-      } // i
-    }
+    entropyMC2 = -copula.computeLogPDF(copula.getSample(1000)).computeMean()[0];
+    std::cout << "entropy (MC)=" << entropyMC2 << std::endl;
   }
+  std::cout<<"Same entropy : ";
+  if (fabs(entropyMC1-entropyMC2)<1e-5)
+    std::cout<<"OK"<<std::endl;
+  else
+    std::cout<<"not OK"<<std::endl;
 }
 
-int main(int argc, char **argv)
-{
-  OT::RandomGenerator::SetSeed(0);
-  OT::ResourceMap::SetAsUnsignedInteger("parallel-threads",1);
-  OT::ResourceMap::SetAsBool("Distribution-Parallel",false);
+int main(int argc, char **argv) {
+  OT::ResourceMap::SetAsUnsignedInteger("parallel-threads", 1);
+  OT::ResourceMap::SetAsBool("Distribution-Parallel", false);
   testOK();
 }
