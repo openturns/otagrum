f6416daee434c22441e98bdcc67be7cb1b64c848
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2019-07-13 15:41:38 +0200

    Small change for master

--- lib/src/ContinuousTTest.cxx
+++ lib/src/ContinuousTTest.cxx
@@ -31,34 +31,28 @@
 
 #include "otagrum/ContinuousTTest.hxx"
 
-namespace OTAGRUM
-{
+namespace OTAGRUM {
 
 ContinuousTTest::ContinuousTTest(const OT::Sample &data, const double alpha)
-  : OT::Object()
-{
+    : OT::Object() {
   setAlpha(alpha);
   data_ = (data.rank() + 0.5) / data.getSize();
 }
 
 OT::UnsignedInteger ContinuousTTest::GetK(const OT::UnsignedInteger size,
-    const OT::UnsignedInteger dimension)
-{
+                                          const OT::UnsignedInteger dimension) {
   return OT::UnsignedInteger(1.0 + std::pow(size, 2.0 / (4.0 + dimension)));
 }
 
 std::string ContinuousTTest::GetKey(const OT::Indices &l,
-                                    const OT::UnsignedInteger k)
-{
+                                    const OT::UnsignedInteger k) {
   //@todo : sorting l before computing the key ?
   return l.__str__() + ":" + std::to_string(k);
 }
 
 OT::Point ContinuousTTest::getLogPDF(const OT::Indices &l,
-                                     const OT::UnsignedInteger k) const
-{
-  if (l.getSize() == 0)
-  {
+                                     const OT::UnsignedInteger k) const {
+  if (l.getSize() == 0) {
     //@todo Is this correct ?
     return OT::Point(1, -std::log(data_.getSize()));
   }
@@ -68,6 +62,7 @@ OT::Point ContinuousTTest::getLogPDF(const OT::Indices &l,
   const auto key = GetKey(l, k);
   if (cache_.exists(key))
     return cache_.get(key);
+
   auto dL = data_.getMarginal(l);
 
   // OT::BernsteinCopulaFactory factory;
@@ -76,13 +71,10 @@ OT::Point ContinuousTTest::getLogPDF(const OT::Indices &l,
   // OT::NormalCopulaFactory factory;
   // auto logPDF = factory.build(dL).computeLogPDF(dL).asPoint();
 
-  gum::Timer t;
-  t.reset();
-
   LOGINFO(OT::OSS() << "Compute log-PDF for k=" << k << ", l=" << l);
   auto logPDF =
-    OT::EmpiricalBernsteinCopula(dL, k, true).computeLogPDF(dL).asPoint();
-  LOGINFO(OT::OSS() << "End of compute log-PDF for k=" << k << ", l=" << l<<" Time : "<<t.step()<<"s");
+      OT::EmpiricalBernsteinCopula(dL, k, true).computeLogPDF(dL).asPoint();
+  LOGINFO(OT::OSS() << "End of compute log-PDF for k=" << k << ", l=" << l);
 
   cache_.set(l.getSize(), key, logPDF);
   return logPDF;
@@ -91,8 +83,7 @@ OT::Point ContinuousTTest::getLogPDF(const OT::Indices &l,
 std::tuple<OT::Point, OT::Point, OT::Point, OT::Point, OT::UnsignedInteger>
 ContinuousTTest::getLogPDFs(const OT::UnsignedInteger Y,
                             const OT::UnsignedInteger Z,
-                            const OT::Indices &X) const
-{
+                            const OT::Indices &X) const {
   //@todo how to be smart for k ?
   // k =BernsteinCopulaFactory::ComputeLogLikelihoodBinNumber(sample,2);
   OT::UnsignedInteger k = GetK(data_.getSize(), X.getSize() + 2);
@@ -103,25 +94,15 @@ ContinuousTTest::getLogPDFs(const OT::UnsignedInteger Y,
   return std::make_tuple(logPDF1, logPDF2, logPDF3, logPDF4, k);
 }
 
-void ContinuousTTest::setAlpha(const double alpha)
-{
-  alpha_ = alpha;
-}
+void ContinuousTTest::setAlpha(const double alpha) { alpha_ = alpha; }
 
-double ContinuousTTest::getAlpha() const
-{
-  return alpha_;
-}
+double ContinuousTTest::getAlpha() const { return alpha_; }
 
-inline double pPar1MoinsP(const double p)
-{
-  return p * (1.0 - p);
-}
+inline double pPar1MoinsP(const double p) { return p * (1.0 - p); }
 
 double ContinuousTTest::getTTest(const OT::UnsignedInteger Y,
                                  const OT::UnsignedInteger Z,
-                                 const OT::Indices &X) const
-{
+                                 const OT::Indices &X) const {
   OT::UnsignedInteger k = 0;
 
   const auto dY = data_.getMarginal(Y);
@@ -147,20 +128,14 @@ double ContinuousTTest::getTTest(const OT::UnsignedInteger Y,
   double logDenominator = 0.0;
   double yI = 0.0;
   double zI = 0.0;
-  if (d == 0)
-  {
+  if (d == 0) {
     const double fX0 = 1.0;
-    for (unsigned int i = 0; i < N; ++i)
-    {
+    for (unsigned int i = 0; i < N; ++i) {
       logDenominator = logFYZX[i];
       yI = data_(i, Y);
       zI = data_(i, Z);
-      if ( (logDenominator > smallLog) &&
-	   (yI > small) &&
-	   (yI < 1.0 - small) &&
-	   (zI > small) &&
-	   (zI < 1.0 - small))
-      {
+      if ((logDenominator > smallLog) && (yI > small) && (yI < 1.0 - small) &&
+          (zI > small) && (zI < 1.0 - small)) {
         // dH^2 = (1-sqrt(1 * 1 / (fYZX * 1)))^2
         //      = (1-exp(0.5*log(1 / fYZX))^2
         //      = (-expm1(-0.5*log(fYZX)))^2
@@ -172,91 +147,75 @@ double ContinuousTTest::getTTest(const OT::UnsignedInteger Y,
         B3 += fX0;
       } // logDenominator > smallLog
       else
-	LOGINFO(OT::OSS() << "Skipped contribution i=" << i
-		<< ", logDenominator=" << logDenominator);
+        LOGINFO(OT::OSS() << "Skipped contribution i=" << i
+                          << ", logDenominator=" << logDenominator);
     } // i
   }   // d == 0
-  else if (d == 1)
-  {
-    for (unsigned int i = 0; i < N; ++i)
-    {
+  else if (d == 1) {
+    for (unsigned int i = 0; i < N; ++i) {
       logDenominator = logFYZX[i];
       yI = data_(i, Y);
       zI = data_(i, Z);
-      if ( (logDenominator > smallLog) &&
-	   (yI > small) &&
-	   (yI < 1.0 - small) &&
-	   (zI > small) &&
-	   (zI < 1.0 - small))
-      {
-	double xJ = data_(i, X[0]);
-	if ((xJ > small) &&
-	    (xJ < 1.0 - small))
-	  continue;
+      if ((logDenominator > smallLog) && (yI > small) && (yI < 1.0 - small) &&
+          (zI > small) && (zI < 1.0 - small)) {
+        double xJ = data_(i, X[0]);
+        if ((xJ > small) && (xJ < 1.0 - small))
+          continue;
         double gX = 1.0 / pPar1MoinsP(xJ);
-	gX = std::sqrt(gX);
+        gX = std::sqrt(gX);
         // dH^2 = (1-sqrt(fYX * fZX / (fYZX * 1)))^2
         //      = (1-exp(0.5*log(fYX * fZX / fYZX))^2
         //      = (-expm1(0.5*(log(fYX) + log(fZX) - log(fYZX))))^2
         //      = (expm1(0.5*(log(fYX) + log(fZX) - log(fYZX))))^2
         dH = std::expm1(0.5 * (logFYX[i] + logFZX[i] - logDenominator));
         H += dH * dH;
-        B1 += facteurpi * gX /
-              (std::sqrt(pPar1MoinsP(yI)) * std::exp(logFYX[i]));
-        B2 += facteurpi * gX /
-              (std::sqrt(pPar1MoinsP(zI)) * std::exp(logFZX[i]));
+        B1 +=
+            facteurpi * gX / (std::sqrt(pPar1MoinsP(yI)) * std::exp(logFYX[i]));
+        B2 +=
+            facteurpi * gX / (std::sqrt(pPar1MoinsP(zI)) * std::exp(logFZX[i]));
         B3 += gX;
       } // logDenominator > smallLog
       else
-	LOGDEBUG(OT::OSS() << "Skip contribution i=" << i
-		 << ", logDenominator=" << logDenominator);
+        LOGDEBUG(OT::OSS() << "Skip contribution i=" << i
+                           << ", logDenominator=" << logDenominator);
     } // i
   }   // d == 1
-  else
-  {
-    for (unsigned int i = 0; i < N; ++i)
-    {
+  else {
+    for (unsigned int i = 0; i < N; ++i) {
       logDenominator = logFYZX[i] + logFX[i];
       yI = data_(i, Y);
       zI = data_(i, Z);
-      if ( (logDenominator > smallLog) &&
-	   (yI > small) &&
-	   (yI < 1.0 - small) &&
-	   (zI > small) &&
-	   (zI < 1.0 - small))
-      {
+      if ((logDenominator > smallLog) && (yI > small) && (yI < 1.0 - small) &&
+          (zI > small) && (zI < 1.0 - small)) {
         double gX = 1.0;
-	double xJ = 0.0;
-	OT::Bool isSmall = false;
-        for (unsigned int j = 0; j < d; ++j)
-	  {
-	    xJ = data_(i, X[j]);
-	    if ((xJ > small) &&
-		(xJ < 1.0 - small))
-	      {
-		isSmall = true;
-		break;
-	      }
-	    gX /= pPar1MoinsP(xJ);
-	    gX = std::sqrt(gX);
-	  }
-	if (isSmall)
-	  continue;
+        double xJ = 0.0;
+        OT::Bool isSmall = false;
+        for (unsigned int j = 0; j < d; ++j) {
+          xJ = data_(i, X[j]);
+          if ((xJ > small) && (xJ < 1.0 - small)) {
+            isSmall = true;
+            break;
+          }
+          gX /= pPar1MoinsP(xJ);
+          gX = std::sqrt(gX);
+        }
+        if (isSmall)
+          continue;
         // dH^2 = (1-sqrt(fYX * fZX / (fYZX * fX)))^2
         //      = (1-exp(0.5*log(fYX * fZX / (fYZX * fX)))^2
         //      = (-expm1(0.5*(log(fYX) + log(fZX) - log(fYZX) - log(fX))))^2
         //      = (expm1(0.5*(log(fYX) + log(fZX) - log(fYZX) - log(fX))))^2
         dH = std::expm1(0.5 * (logFYX[i] + logFZX[i] - logDenominator));
         H += dH * dH;
-        B1 += facteurpi * gX /
-              (std::sqrt(pPar1MoinsP(yI)) * std::exp(logFYX[i]));
-        B2 += facteurpi * gX /
-              (std::sqrt(pPar1MoinsP(zI)) * std::exp(logFZX[i]));
+        B1 +=
+            facteurpi * gX / (std::sqrt(pPar1MoinsP(yI)) * std::exp(logFYX[i]));
+        B2 +=
+            facteurpi * gX / (std::sqrt(pPar1MoinsP(zI)) * std::exp(logFZX[i]));
         B3 += std::exp(logFX[i]) * gX;
       } // logDenominator > smallLog
       else
-	LOGDEBUG(OT::OSS() << "Skip contribution i=" << i
-		 << ", logDenominator=" << logDenominator);
+        LOGDEBUG(OT::OSS() << "Skip contribution i=" << i
+                           << ", logDenominator=" << logDenominator);
     } // i
   }   // d > 0
   // mean
@@ -271,15 +230,14 @@ double ContinuousTTest::getTTest(const OT::UnsignedInteger Y,
   auto T = std::pow(1.0 / k, 0.5 * d + 1.0) / sigma;
   T *= 4 * H * N - pow(k, 0.5 * d) * (C1 * k + (B1 + B2) * std::sqrt(k) + B3);
   LOGINFO(OT::OSS() << "Y=" << Y << ", Z=" << Z << ", X=" << X << ", T=" << T
-          << ", H=" << H << ", B1=" << B1 << ", B2=" << B2
-          << ", B3=" << B3);
+                    << ", H=" << H << ", B1=" << B1 << ", B2=" << B2
+                    << ", B3=" << B3);
   return T;
 }
 
 double ContinuousTTest::getTTestWithoutCorrections(OT::UnsignedInteger Y,
-    OT::UnsignedInteger Z,
-    const OT::Indices &X) const
-{
+                                                   OT::UnsignedInteger Z,
+                                                   const OT::Indices &X) const {
   OT::UnsignedInteger k = 0;
 
   const auto dY = data_.getMarginal(Y);
@@ -305,31 +263,28 @@ double ContinuousTTest::getTTestWithoutCorrections(OT::UnsignedInteger Y,
   else
     for (unsigned int i = 0; i < N; ++i)
       H += std::pow(
-             std::expm1(0.5 * logFYX[i] + logFZX[i] - logFYZX[i] - logFX[i]), 2.0);
+          std::expm1(0.5 * logFYX[i] + logFZX[i] - logFYZX[i] - logFX[i]), 2.0);
   const auto T =
-    4 * H * N - pow(k, 0.5 * d) / std::pow(k, 0.5 * d + 1.0) / sigma;
+      4 * H * N - pow(k, 0.5 * d) / std::pow(k, 0.5 * d + 1.0) / sigma;
   LOGINFO(OT::OSS() << "Y=" << Y << ", Z=" << Z << ", X=" << X << ", T=" << T
-          << ", H=" << H);
+                    << ", H=" << H);
   return T;
 }
 
 std::tuple<double, double, bool>
 ContinuousTTest::isIndep(const OT::UnsignedInteger Y,
                          const OT::UnsignedInteger Z,
-                         const OT::Indices &X) const
-{
+                         const OT::Indices &X) const {
   return ContinuousTTest::isIndepFromTest(getTTest(Y, Z, X), alpha_);
 }
 
 std::tuple<double, double, bool>
-ContinuousTTest::isIndepFromTest(const double t, const double alpha)
-{
+ContinuousTTest::isIndepFromTest(const double t, const double alpha) {
   const double p = 2.0 * OT::DistFunc::pNormal(-std::abs(t));
   return std::make_tuple(t, p, p >= alpha);
 }
 
-std::string ContinuousTTest::__str__(const std::string &offset) const
-{
+std::string ContinuousTTest::__str__(const std::string &offset) const {
   std::stringstream ss;
   ss << offset << "Data dimension : " << data_.getDimension() << std::endl;
   ss << offset << "Data size : " << data_.getSize() << std::endl;
@@ -339,23 +294,17 @@ std::string ContinuousTTest::__str__(const std::string &offset) const
   return ss.str();
 }
 
-void ContinuousTTest::clearCache() const
-{
-  cache_.clear();
-}
+void ContinuousTTest::clearCache() const { cache_.clear(); }
 
-void ContinuousTTest::clearCacheLevel(const OT::UnsignedInteger level) const
-{
+void ContinuousTTest::clearCacheLevel(const OT::UnsignedInteger level) const {
   cache_.clearLevel(level);
 }
 
-OT::UnsignedInteger ContinuousTTest::getDimension() const
-{
+OT::UnsignedInteger ContinuousTTest::getDimension() const {
   return data_.getDimension();
 }
 
-OT::Description ContinuousTTest::getDataDescription() const
-{
+OT::Description ContinuousTTest::getDataDescription() const {
   return data_.getDescription();
 }
 } // namespace OTAGRUM
