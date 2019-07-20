25e14b8b6a981a1ebda44b3afc9774d5dbaa6e8b
regislebrun <regis.lebrun@m4x.org>
2019-07-20 15:06:54 +0200

    Fixed Utils

--- lib/src/otagrum/Utils.hxx
+++ lib/src/otagrum/Utils.hxx
@@ -55,13 +55,35 @@ public:
 
   static OT::Indices FromNodeSet(const gum::NodeSet &clique);
 
-  static inline float FastPow2(float p);
-  
-  static inline float FastExp(float p);
-  
-  static inline float FasterPow2(float p);
-  
-  static inline float FasterExp(float p);
+  // Underflow of exponential is common practice in numerical routines,
+  // so handle it here.
+  static inline float FastPow2(float p)
+  {
+    float offset = (p < 0) ? 1.0f : 0.0f;
+    float clipp = (p < -126) ? -126.0f : p;
+    int w = clipp;
+    float z = clipp - w + offset;
+    union { uint32_t i; float f; } v = { static_cast<uint32_t> ( (1 << 23) * (clipp + 121.2740575f + 27.7280233f / (4.84252568f - z) - 1.49012907f * z) ) };
+    
+    return v.f;
+  }
+
+  static inline float FastExp(float p)
+  {
+    return FastPow2 (1.442695040f * p);
+  }
+
+  static inline float FasterPow2(float p)
+  {
+    float clipp = (p < -126) ? -126.0f : p;
+    union { uint32_t i; float f; } v = { static_cast<uint32_t> ( (1 << 23) * (clipp + 126.94269504f) ) };
+    return v.f;
+  }
+
+  static inline float FasterExp(float p)
+  {
+    return FasterPow2 (1.442695040f * p);
+  }
   
 private:
   Utils();
