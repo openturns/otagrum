2352810fcd118cf8f654b4a4fd44749f33f9ba94
Julien Schueller <schueller@phimeca.com>
2023-02-15 11:50:00 +0100

    Sphinx: Require 1.8

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -2,21 +2,22 @@
 
 set -xe
 
-uid=$1
-gid=$2
-
 cd /tmp
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=~/.local \
+      -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
       -DCMAKE_CXX_FLAGS="-Wall -Wextra -Werror -D_GLIBCXX_ASSERTIONS" \
+      -DSPHINX_FLAGS="-W -T -j4" \
       /io
 make install
 make tests
-ctest --output-on-failure --timeout 50 ${MAKEFLAGS}
+ctest --output-on-failure --timeout 100 ${MAKEFLAGS}
 
+uid=$1
+gid=$2
 if test -n "${uid}" -a -n "${gid}"
 then
-  sudo cp -rv ~/.local/share/otagrum/doc/html /io
+  sudo cp -r ~/.local/share/*/doc/html /io
   sudo chown -R ${uid}:${gid} /io/html
 fi
 
