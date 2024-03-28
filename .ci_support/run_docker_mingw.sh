352af118dc79550f9e141448e807943a37bbef41
Julien Schueller <schueller@phimeca.com>
2024-03-28 17:11:56 +0100

    CI: Drop CMAKE_UNITY_BUILD_BATCH_SIZE

--- .ci_support/run_docker_mingw.sh
+++ .ci_support/run_docker_mingw.sh
@@ -8,10 +8,10 @@ mkdir build && cd build
 PREFIX=${PWD}/install
 CXXFLAGS="-Wall -Wextra -D_GLIBCXX_ASSERTIONS" ${ARCH}-w64-mingw32-cmake \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_UNITY_BUILD=ON \
   -DPython_INCLUDE_DIR=${MINGW_PREFIX}/include/python${PYMAJMIN} \
   -DPython_LIBRARY=${MINGW_PREFIX}/lib/libpython${PYMAJMIN}.dll.a \
   -DPython_EXECUTABLE=/usr/bin/${ARCH}-w64-mingw32-python${PYMAJMIN}-bin \
-  -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
   /io
 make install
 ${ARCH}-w64-mingw32-strip --strip-unneeded ${PREFIX}/bin/*.dll ${PREFIX}/Lib/site-packages/*/*.pyd
