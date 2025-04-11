cb3cb29bbcba7cba0274cfdac0539885703b69c1
Julien Schueller <schueller@phimeca.com>
2025-04-11 11:06:31 +0200

    CI: coverage

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -4,14 +4,21 @@ set -xe
 
 cd /tmp
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/.local \
+cmake -DCMAKE_INSTALL_PREFIX=${HOME}/.local \
       -DCMAKE_UNITY_BUILD=ON \
-      -DCMAKE_CXX_FLAGS="-Wall -Wextra -Werror -Wno-unused-parameter -D_GLIBCXX_ASSERTIONS" \
+      -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Wshadow -Werror -D_GLIBCXX_ASSERTIONS --coverage" \
+      -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter -Wno-shadow" \
       -DUSE_SPHINX=ON -DSPHINX_FLAGS="-W -T -j4" \
       /io
 make install
 make tests
 ctest --output-on-failure --timeout 100 ${MAKEFLAGS}
+
+# coverage
+gcov `find lib/src/ -name "*.gcno"`
+lcov --capture --directory . --output-file coverage.info --include "*.cxx"
+genhtml --output-directory coverage coverage.info
+cp -v coverage.info coverage
 
 uid=$1
 gid=$2
