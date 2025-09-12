66f7be117ddaae745f1e7d668b627be95bfe2a61
Julien Schueller <schueller@phimeca.com>
2025-09-12 16:27:56 +0200

    CI: Use build_locally.sh

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -3,13 +3,13 @@
 set -xe
 
 cd /tmp
-mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=${HOME}/.local \
       -DCMAKE_UNITY_BUILD=ON \
-      -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Wshadow -Werror -D_GLIBCXX_ASSERTIONS --coverage" \
+      -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Wshadow -Wsuggest-override -Werror -D_GLIBCXX_ASSERTIONS --coverage" \
       -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter -Wno-shadow" \
       -DUSE_SPHINX=ON -DSPHINX_FLAGS="-W -T -j4" \
-      /io
+      -B build /io
+cd build
 make install
 make tests
 ctest --output-on-failure --timeout 100 ${MAKEFLAGS}
@@ -20,12 +20,11 @@ lcov --capture --directory . --output-file coverage.info --include "*.cxx"
 genhtml --ignore-errors inconsistent --output-directory coverage coverage.info
 cp -v coverage.info coverage
 
-uid=$1
-gid=$2
-if test -n "${uid}" -a -n "${gid}"
+UID_GID=$1
+if test -n "${UID_GID}"
 then
+  sudo chown -R ${uid}:${gid} ~/.local/share/doc/*/html
   sudo cp -r ~/.local/share/doc/*/html /io
-  sudo chown -R ${uid}:${gid} /io/html
 fi
 
 python /io/python/doc/examples/torque_model.py
